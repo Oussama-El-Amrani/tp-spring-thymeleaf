2b246ad7a1f7b6e83fb5a523f39340fa901a1025
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-03-17 10:53:18 +0000

    feat: show all patients in index page

--- src/main/java/io/oussamaib0/tpspringthymeleaf/web/PatientController.java
+++ src/main/java/io/oussamaib0/tpspringthymeleaf/web/PatientController.java
@@ -3,6 +3,7 @@ package io.oussamaib0.tpspringthymeleaf.web;
 import io.oussamaib0.tpspringthymeleaf.services.IPatientService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,7 +12,8 @@ public class PatientController {
     private final IPatientService patientService;
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("patients", patientService.getAllPatients());
         return "index";
     }
 }
