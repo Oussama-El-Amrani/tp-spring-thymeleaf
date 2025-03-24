637e9a7d1598144456f295a22b954cce235f13de
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-03-24 14:28:52 +0000

    feat: Add the possibility to delete a patient

--- src/main/java/io/oussamaib0/tpspringthymeleaf/web/PatientController.java
+++ src/main/java/io/oussamaib0/tpspringthymeleaf/web/PatientController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,16 +16,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PatientController {
     private final IPatientService patientService;
 
-     @GetMapping("/")
+    @GetMapping("/")
     public String home() {
         return "redirect:/index";
     }
 
     @GetMapping("/index")
     public String index(Model model,
-                       @RequestParam(name = "page", defaultValue = "0") int page,
-                       @RequestParam(name = "size", defaultValue = "5") int size,
-                       @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "5") int size,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword) {
         Page<Patient> pagePatients = patientService.findByNameContainsIgnoreCase(keyword, PageRequest.of(page, size));
         model.addAttribute("listPatients", pagePatients.getContent());
         model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
@@ -32,5 +33,15 @@ public class PatientController {
         model.addAttribute("totalPages", pagePatients.getTotalPages());
         model.addAttribute("keyword", keyword);
         return "patients";
+    }
+
+    @GetMapping("/patient-delete/{id}")
+    public String delete(
+            @PathVariable Long id,
+            @RequestParam(name = "keyword", defaultValue = "") String keyword,
+            @RequestParam(name = "page", defaultValue = "0") int page
+    ) {
+        patientService.deletePatient(id);
+        return "redirect:/index?page=" + page + "&keyword=" + keyword;
     }
 }
